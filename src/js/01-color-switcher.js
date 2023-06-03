c33c6c51e4d31b828372acb5b29f54a90ffbd317
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-06-03 22:33:30 +0100

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const bodyEl = document.querySelector('body');
+const btnStartEl = document.querySelector('[data-start]');
+const btnStopEl = document.querySelector('[data-stop]');
+
+const colorSwitcher = {
+  intervalID: null,
+  start() {
+    this.intervalID = setInterval(() => {
+      bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.intervalID);
+  },
+};
+
+btnStartEl.addEventListener('click', () => {
+  colorSwitcher.start();
+  btnStartEl.disabled = true;
+  btnStopEl.disabled = false;
+});
+
+btnStopEl.addEventListener('click', () => {
+  colorSwitcher.stop();
+  btnStartEl.disabled = false;
+  btnStopEl.disabled = true;
+});
